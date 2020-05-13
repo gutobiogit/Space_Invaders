143ac38cbc290ec0bfebbef15a5da364c4f0b680
Guto <gutobioit@gmail.com>
2020-05-13 11:34:37 +0200

    Initial commit

---
+++ Assets/Scripts/Missile.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Missile : MonoBehaviour
+{
+    public float speed1 = 2f;
+    public float speed2 = 10f;
+    private ParticleSystem ps;
+    public Text Score;
+    public GameObject Explosion;
+    private int sub_score;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ps = Object.FindObjectOfType<ParticleSystem>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector2.up * speed1 * Time.deltaTime);
+        if (transform.position.y > -3.0f)
+        {
+            ps.Play();
+            transform.Translate(Vector2.up * speed2 * Time.deltaTime);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if ((other.CompareTag("Enemy4")) && (other.isActiveAndEnabled))
+        {
+            GameObject explode = Instantiate(Explosion) as GameObject;
+            explode.transform.position = transform.position;
+            sub_score = int.Parse(Score.text);
+            sub_score += 10;
+            Score.text = sub_score.ToString();
+            Destroy(explode, .2f);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+        if ((other.CompareTag("Enemy3")) && (other.isActiveAndEnabled))
+        {
+            GameObject explode = Instantiate(Explosion) as GameObject;
+            explode.transform.position = transform.position;
+            sub_score = int.Parse(Score.text);
+            sub_score += 25;
+            Score.text = sub_score.ToString();
+            Destroy(explode, .2f);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+        if ((other.CompareTag("Enemy")) && (other.isActiveAndEnabled) )
+        {
+            GameObject explode = Instantiate(Explosion) as GameObject;
+            explode.transform.position = transform.position;
+            sub_score =int.Parse(Score.text);
+            sub_score += 50;
+            Score.text = sub_score.ToString();
+            Destroy(explode, .2f);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+
+        if ((other.CompareTag("Shield")) && (other.isActiveAndEnabled) )
+        {
+            GameObject explode = Instantiate(Explosion) as GameObject;
+            explode.transform.position = transform.position;
+            Destroy(explode, .2f);
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+        
+
+}
---
+++ Assets/Scripts/Tank.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour
+{
+    public float speed = 2f;
+    public GameObject Missile;
+    private GameObject missile_prefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ((Input.GetKey(KeyCode.RightArrow)) && (transform.position.x < 8.6f))
+        {   
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+            Missile.transform.Translate(Vector2.right * speed * Time.deltaTime);
+        }
+        
+        if ((Input.GetKey(KeyCode.LeftArrow)) && (transform.position.x > -8.9f))
+        {   
+            transform.Translate(Vector2.left * speed * Time.deltaTime);
+            Missile.transform.Translate(Vector2.left * speed * Time.deltaTime);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            missile_prefab = Instantiate(Missile, new Vector3(transform.position.x+0.14f, -4.49f, -2f), Quaternion.identity) as GameObject;
+            missile_prefab.AddComponent<Missile>();
+            Destroy(missile_prefab, 2f);
+        }
+    }
+
+}
